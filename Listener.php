32a333cc787abb36d5b9ded145a4096512d14470
Simon Hampel <simon@hampelgroup.com>
2020-10-07 11:06:55 +1100

    first working version

---
+++ Listener.php
@@ -0,0 +1,74 @@
+<?php namespace Hampel\ItemsThisPage;
+
+class Listener
+{
+	public static function templaterGlobalData(\XF\App $app, array &$data, $reply)
+	{
+		if ($app instanceof \XF\Pub\App && $reply && $reply instanceof \XF\Mvc\Reply\View)
+		{
+			$params = $reply->getParams();
+			$template = $reply->getTemplateName();
+
+			switch ($template)
+			{
+				case 'thread_view': // fall through
+				case 'xfrm_thread_view_type_resource':
+
+					$data['itemsThisPage'] = self::itemsPerPage($params['page'], $params['totalPosts'], $params['perPage']);
+					break;
+
+				case 'forum_view': // fall through
+				case 'find_threads_list':
+				case 'watched_threads_list':
+				case 'member_list':
+				case 'online_list':
+				case 'xfrm_overview':
+				case 'xfrm_category_view':
+				case 'xfrm_latest_reviews':
+				case 'xfrm_author_view':
+
+					$data['itemsThisPage'] = self::itemsPerPage($params['page'], $params['total'], $params['perPage']);
+					break;
+
+				case 'xfmg_media_index': // fall through
+				case 'xfmg_media_view':
+				case 'xfmg_category_view':
+				case 'xfmg_album_index':
+				case 'xfmg_media_user_index':
+				case 'xfmg_album_user_index':
+
+					$data['itemsThisPage'] = self::itemsPerPage($params['page'], $params['totalItems'], $params['perPage']);
+					break;
+
+				default:
+					break;
+			}
+		}
+	}
+
+	public static function itemsPerPage($page, $total, $perPage)
+	{
+		if ($total == 0)
+		{
+			return 0;
+		}
+
+		$totalPages = intval(ceil($total / $perPage));
+
+		if ($page < $totalPages)
+		{
+			return intval($perPage);
+		}
+		else
+		{
+			if ($total % $perPage == 0)
+			{
+				return intval($perPage);
+			}
+			else
+			{
+				return $total % $perPage;
+			}
+		}
+	}
+}
