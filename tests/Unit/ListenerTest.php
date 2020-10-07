9bd858d6ce25fd671640da0c44df580581575e0c
Simon Hampel <simon@hampelgroup.com>
2020-10-07 21:41:01 +1100

    unit test updates

--- tests/Unit/ListenerTest.php
+++ tests/Unit/ListenerTest.php
@@ -7,13 +7,13 @@ class ListenerTest extends TestCase
 {
     public function testItemsPerPage()
     {
-        $this->assertEquals(0, Listener::itemsPerPage(1, 0, 5));
-        $this->assertEquals(1, Listener::itemsPerPage(1, 1, 5));
-        $this->assertEquals(5, Listener::itemsPerPage(1, 5, 5)); //
-        $this->assertEquals(5, Listener::itemsPerPage(1, 6, 5));
-        $this->assertEquals(1, Listener::itemsPerPage(2, 6, 5));
-        $this->assertEquals(2, Listener::itemsPerPage(2, 7, 5));
-        $this->assertEquals(5, Listener::itemsPerPage(2, 10, 5));
-        $this->assertEquals(5, Listener::itemsPerPage(2, 11, 5));
+        $this->assertEquals(0, Listener::itemsThisPage(1, 0, 5));
+        $this->assertEquals(1, Listener::itemsThisPage(1, 1, 5));
+        $this->assertEquals(5, Listener::itemsThisPage(1, 5, 5)); //
+        $this->assertEquals(5, Listener::itemsThisPage(1, 6, 5));
+        $this->assertEquals(1, Listener::itemsThisPage(2, 6, 5));
+        $this->assertEquals(2, Listener::itemsThisPage(2, 7, 5));
+        $this->assertEquals(5, Listener::itemsThisPage(2, 10, 5));
+        $this->assertEquals(5, Listener::itemsThisPage(2, 11, 5));
     }
 }
